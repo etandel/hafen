ed57493c2ba286b5f20746224b78c21251d32c68
Elias Tandel <elias.tandel@gmail.com>
2021-09-18 21:10:53 +0200

    Add Trainer.get_random_article_trainer()

--- lib/hafen/trainer.ex
+++ lib/hafen/trainer.ex
@@ -3,29 +3,61 @@ defmodule Hafen.Trainer do
   Trainers
   """
 
+  alias Hafen.Corpora
   alias Hafen.Corpora.Text
   alias Hafen.Article
 
-  def get_article_trainer(%Text{raw_text: raw_text}) do
+  @enforce_keys [:text, :sentence_index, :splitted_sentence]
+  defstruct [:text, :sentence_index, :splitted_sentence]
+
+  @type t() :: %__MODULE__{
+          text: Text.t(),
+          sentence_index: integer(),
+          splitted_sentence: list(String.t())
+        }
+
+  def get_article_trainer(%Text{raw_text: raw_text} = text) do
     splitted =
       raw_text
       |> String.split(".")
+      |> Stream.with_index()
       |> Enum.shuffle()
-      |> Stream.map(fn sentence ->
+      |> Stream.map(fn {sentence, idx} ->
         splitted = Article.split_on_articles(sentence)
 
         if length(splitted) == 1 or Enum.all?(splitted, &(String.trim(&1) == "")) do
           nil
         else
-          splitted
+          {splitted, idx}
         end
       end)
       |> Stream.filter(&(&1 != nil))
       |> Enum.at(0)
 
     case splitted do
-      nil -> {:error, "Cannot get trainer for text"}
-      s -> {:ok, s}
+      nil ->
+        {:error, "Cannot get trainer for text"}
+
+      {splitted_sentence, idx} ->
+        tr = %__MODULE__{
+          text: text,
+          sentence_index: idx,
+          splitted_sentence: splitted_sentence
+        }
+
+        {:ok, tr}
+    end
+  end
+
+  def get_random_article_trainer() do
+    trainer = Corpora.get_random_text() |> get_article_trainer()
+
+    case trainer do
+      {:error, _} ->
+        get_random_article_trainer()
+
+      {:ok, trainer} ->
+        trainer
     end
   end
 end
--- test/hafen/trainer_test.exs
+++ test/hafen/trainer_test.exs
@@ -1,10 +1,25 @@
 defmodule Hafen.TrainerText do
-  use ExUnit.Case
+  use Hafen.DataCase
 
   alias Hafen.Corpora.Text
   alias Hafen.Trainer
 
+  defp make_expected(expected_text, expected_splitted, expected_index \\ 0) do
+    %Trainer{
+      text: expected_text,
+      splitted_sentence: expected_splitted,
+      sentence_index: expected_index
+    }
+  end
+
   describe "get_article_trainer" do
+    defp assert_ok(raw_text, expected_splitted, expected_index \\ 0) do
+      t = %Text{raw_text: raw_text}
+
+      expected = make_expected(t, expected_splitted, expected_index)
+      assert Trainer.get_article_trainer(t) == {:ok, expected}
+    end
+
     test "get_article_trainer/1 with empty string error" do
       t = %Text{raw_text: ""}
       assert {:error, _} = Trainer.get_article_trainer(t)
@@ -27,16 +42,12 @@ defmodule Hafen.TrainerText do
     end
 
     test "get_article_trainer/1 with sentence with article and one word returns ok" do
-      t = %Text{raw_text: "die frau"}
-      assert {:ok, ["", " frau"]} = Trainer.get_article_trainer(t)
-
-      t = %Text{raw_text: "frau die"}
-      assert {:ok, ["frau ", ""]} = Trainer.get_article_trainer(t)
+      assert_ok("die frau", ["", " frau"])
+      assert_ok("frau die", ["frau ", ""])
     end
 
     test "get_article_trainer/1 with sentence with multiple articles returns ok" do
-      t = %Text{raw_text: "die frau der"}
-      assert {:ok, ["", " frau ", ""]} = Trainer.get_article_trainer(t)
+      assert_ok("die frau der", ["", " frau ", ""])
     end
 
     test "get_article_trainer/1 with only bad sentences returns error" do
@@ -45,17 +56,33 @@ defmodule Hafen.TrainerText do
     end
 
     test "get_article_trainer/1 with 1 bad and 1 good sentence returns ok" do
-      t = %Text{raw_text: "die. die frau"}
-      assert {:ok, [" ", " frau"]} = Trainer.get_article_trainer(t)
-
-      t = %Text{raw_text: "die frau. die."}
-      assert {:ok, ["", " frau"]} = Trainer.get_article_trainer(t)
+      assert_ok("die. die frau", [" ", " frau"], 1)
+      assert_ok("die frau. die.", ["", " frau"], 0)
     end
 
     test "get_article_trainer/1 with 2 good sentences returns ok" do
       t = %Text{raw_text: "die frau. der man."}
-      got = Trainer.get_article_trainer(t)
-      assert got == {:ok, ["", " frau"]} or got == {:ok, [" ", " man"]}
+      {:ok, got} = Trainer.get_article_trainer(t)
+      assert got in [make_expected(t, ["", " frau"], 0), make_expected(t, [" ", " man"], 1)]
+    end
+  end
+
+  describe "get_random_article_trainer" do
+    alias Hafen.Corpora
+
+    test "get_random_article_trainer/0 gets random text" do
+      {:ok, corpus} = Corpora.create_corpus(%{reference: "x"})
+      {:ok, t} = Corpora.create_text(%{raw_text: "die frau.", corpus_id: corpus.id})
+
+      assert Trainer.get_random_article_trainer() == make_expected(t, ["", " frau"])
+    end
+
+    test "get_random_article_trainer/0 retries until a trainer is generated" do
+      {:ok, corpus} = Corpora.create_corpus(%{reference: "x"})
+      {:ok, _bad_t} = Corpora.create_text(%{raw_text: ".", corpus_id: corpus.id})
+      {:ok, t} = Corpora.create_text(%{raw_text: "die frau.", corpus_id: corpus.id})
+
+      assert Trainer.get_random_article_trainer() == make_expected(t, ["", " frau"])
     end
   end
 end
